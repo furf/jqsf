14b3c2020f2520bec8fc6eb73fbeb947173290df
furf <dfurfero@hugeinc.com>
2015-06-20 02:42:26 -0700

    add previews page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '#JQSF15' });
+});
+
+router.get('/previews', function(req, res, next) {
+  res.render('previews', { title: '#JQSF15 Previews' });
 });
 
 router.get('/portrait', function(req, res, next) {
